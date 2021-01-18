18f10ecbbe7b6891082c6896d9dad337a1ff15bb
antmarcab4 <antmarcab4@alum.us.es>
2021-01-18 01:42:04 +0000

    Type: Refactor, Subject: Intento dearreglo para el despliegue autmático en heroku

--- decide/decide/base_settings.py
+++ decide/decide/base_settings.py
@@ -165,10 +165,6 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
-BASEURL= "https://guadalhorce-heroku.herokuapp.com"
-
-APIS = {}
-
 try:
     from local_settings import *
 except ImportError:
@@ -184,6 +180,3 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
-
-import django_heroku
-django_heroku.settings(locals())
